4118d3e147f2b8d7f101ff69ba5c25510d1c2abb
Fernando <fernando.vera@adevcom.cl>
2019-01-23 11:42:20 -0300

    comments

--- src/main/java/com/bolsadeideas/springboot/backned/apirest/models/services/IClienteService.java
+++ src/main/java/com/bolsadeideas/springboot/backned/apirest/models/services/IClienteService.java
@@ -6,6 +6,7 @@ import com.bolsadeideas.springboot.backned.apirest.models.entity.Cliente;
 
 public interface IClienteService {
 
+	/*se definen los metodos para luego implementarlos en el controlador */
 
 	public List<Cliente> findAll();
 	
